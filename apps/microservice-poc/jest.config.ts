3b3a839955906df13071178710195dd932239eb9
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2022-11-01 16:16:47 +0100

    Initial commit

---
+++ apps/microservice-poc/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'microservice-poc',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/microservice-poc',
+};
